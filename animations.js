b8710ffe17353d736391dcbe0b3bf7929ae9fbfb
Louie Bertoncin <Louie.Bertoncin@cerner.com>
2015-07-02 16:19:42 -0500

    Fixed header scrolling

--- animations.js
+++ animations.js
@@ -5,7 +5,7 @@ var smoothScrolling = $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 50
+          scrollTop: target.offset().top - $(".cbp-af-header").height()
         }, 750);
         return false;
       }
