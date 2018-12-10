9683fa397c1b25cb3a83d6b78e5adfa9f63f3e1f
louiebert <louiebertoncin@gmail.com>
2018-12-09 21:28:23 -0600

    Added tslint and config

---
+++ docs/precache-manifest.d28adc091ff0ed7a1e6bc5b3683e3fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eda784520393e23c87f8",
+    "url": "/static/js/main.eda78452.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "eda784520393e23c87f8",
+    "url": "/static/css/main.a5b63cce.chunk.css"
+  },
+  {
+    "revision": "0fdcf1161366eec78c0ca04572dff857",
+    "url": "/index.html"
+  }
+];
