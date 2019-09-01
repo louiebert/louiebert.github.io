dcf3325468ef0e4de5225c99965519572ded7aaa
louiebert <louiebertoncin@gmail.com>
2019-09-01 12:09:26 -0500

    CSS WORKS NOW

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import styled, { createGlobalStyle } from 'styled-components/macro'
+import { createGlobalStyle } from 'styled-components/macro'
 import Background from './Background'
 
 class App extends Component {
   render() {
     return (
-      <AppComponent>
+      <Background>
         <GlobalStyle />
-        <Background>{/* actual app code will go here */}</Background>
-      </AppComponent>
+        {/* actual app code will go here */}
+      </Background>
     )
   }
 }
@@ -24,13 +24,6 @@ const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-`
-
-const AppComponent = styled.div`
-  position: relative;
-  text-align: center;
-  /* TODO: remove */
-  height: 200vh;
 `
 
 export default App
--- src/Background.tsx
+++ src/Background.tsx
@@ -4,107 +4,84 @@ import { ReactComponent as Mountain1 } from './images/Mountain1.svg'
 import { ReactComponent as Mountain2 } from './images/Mountain2.svg'
 import { ReactComponent as Mountain3 } from './images/Mountain3.svg'
 import { ReactComponent as Mountain4 } from './images/Mountain4.svg'
-
-type State = {
-  yOffset: number
-}
-
-class Background extends Component<{}, State> {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      yOffset: 0
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.backgroundScroll)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.backgroundScroll)
-  }
-
-  backgroundScroll = () => {
-    this.setState({ yOffset: window.pageYOffset })
-  }
-
-  calcYPos = (speed: number) => {
-    return -((this.state.yOffset * speed) / 100)
-  }
-
+class Background extends Component {
   render() {
     return (
       <BackgroundComponent>
         <ParallaxLayer
           inputColor="hsla(0, 0%, 15%, 1.0)"
-          backgroundImageUrl="./images/Mountain1.svg"
-        />
-        {/*         
-        <ParallaxLayer
-          className="parallax"
-          inputColor="hsla(0, 0%, 20%, 1.0)"
-          style={{ transform: `translate3d(0px, ${this.calcYPos(20)}px, 0px)` }}
+          translateZ={10}
+          marginTop={10}
         >
-          <Mountain3 />
+          <Mountain1 />
         </ParallaxLayer>
         <ParallaxLayer
-          className="parallax"
-          inputColor="hsla(0, 0%, 28%, 1.0)"
-          style={{ transform: `translate3d(0px, ${this.calcYPos(32)}px, 0px)` }}
+          inputColor="hsla(0, 0%, 20%, 1.0)"
+          translateZ={7}
+          marginTop={25}
         >
           <Mountain2 />
         </ParallaxLayer>
         <ParallaxLayer
-          className="parallax"
-          inputColor="hsla(0, 0%, 40%, 1.0)"
-          style={{ transform: `translate3d(0px, ${this.calcYPos(48)}px, 0px)` }}
+          inputColor="hsla(0, 0%, 28%, 1.0)"
+          translateZ={3}
+          marginTop={35}
         >
-          <Mountain1 />
-        </ParallaxLayer> 
-        */}
+          <Mountain3 />
+        </ParallaxLayer>
+        <ParallaxLayer
+          inputColor="hsla(0, 0%, 40%, 1.0)"
+          translateZ={1}
+          marginTop={60}
+        >
+          <Mountain4 />
+        </ParallaxLayer>
 
+        <PageContent />
         {this.props.children}
       </BackgroundComponent>
     )
   }
 }
 
+const perspective = 1
+
 const BackgroundComponent = styled.div`
-  height: 100vh;
+  height: 100vmin;
   overflow-x: hidden;
   overflow-y: auto;
-  perspective: 2px;
+  perspective: ${perspective}px;
 `
 
 const ParallaxLayer = styled.div<{
   inputColor: string
-  backgroundImageUrl: string
+  translateZ: number
+  marginTop?: number
 }>`
-  position: relative;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: none;
+  justify-content: center;
 
-  & ::after {
-    content: ' ';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-
-    transform: translateZ(-1px) scale(1.5px);
-    background-size: 100%;
-    z-index: -1;
-
-    background-image: url(${props => props.backgroundImageUrl});
-  }
+  transform: translateZ(-${props => props.translateZ}px)
+    scale(${props => 1 + props.translateZ / perspective});
+  z-index: -1;
 
   & > svg {
+    margin-top: ${props => props.marginTop}vmin;
+    height: 50vw;
     width: 100vw;
-
     polygon {
       fill: ${props => props.inputColor};
     }
   }
+`
+
+const PageContent = styled.div`
+  height: 6000px;
 `
 
 export default Background
