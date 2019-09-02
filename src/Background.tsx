ce231096428ddbc3c4f4d478d65bd86b2ca9086e
louiebert <louiebertoncin@gmail.com>
2019-09-01 22:46:31 -0500

    SIGNATURE IS LIVE

--- src/Background.tsx
+++ src/Background.tsx
@@ -4,38 +4,53 @@ import { ReactComponent as Mountain1 } from './images/Mountain1.svg'
 import { ReactComponent as Mountain2 } from './images/Mountain2.svg'
 import { ReactComponent as Mountain3 } from './images/Mountain3.svg'
 import { ReactComponent as Mountain4 } from './images/Mountain4.svg'
+import { ReactComponent as Signature } from './images/Signature.svg'
 class Background extends Component {
   render() {
     return (
       <BackgroundComponent>
         <ParallaxLayer
-          inputColor="hsla(0, 0%, 15%, 1.0)"
+          inputColor="hsla(0, 0%, 40%, 1.0)"
           translateZ={10}
           marginTop={10}
         >
           <Mountain1 />
         </ParallaxLayer>
         <ParallaxLayer
-          inputColor="hsla(0, 0%, 20%, 1.0)"
+          inputColor="hsla(0, 0%, 28%, 1.0)"
           translateZ={7}
           marginTop={25}
         >
           <Mountain2 />
         </ParallaxLayer>
         <ParallaxLayer
-          inputColor="hsla(0, 0%, 28%, 1.0)"
-          translateZ={3}
-          marginTop={35}
+          inputColor="hsla(0, 0%, 20%, 1.0)"
+          translateZ={5}
+          marginTop={32}
+          height={25}
+        >
+          <Signature />
+        </ParallaxLayer>
+        <ParallaxLayer
+          inputColor="hsla(0, 0%, 20%, 1.0)"
+          translateZ={4}
+          marginTop={42}
         >
           <Mountain3 />
         </ParallaxLayer>
         <ParallaxLayer
-          inputColor="hsla(0, 0%, 40%, 1.0)"
-          translateZ={1}
-          marginTop={60}
+          inputColor="hsla(0, 0%, 15%, 1.0)"
+          translateZ={2}
+          marginTop={62}
         >
           <Mountain4 />
         </ParallaxLayer>
+        <ParallaxRect
+          inputColor="hsla(0, 0%, 15%, 1.0)"
+          translateZ={2}
+          marginTop={452}
+          height={400}
+        />
 
         <PageContent />
         {this.props.children}
@@ -56,7 +71,8 @@ const BackgroundComponent = styled.div`
 const ParallaxLayer = styled.div<{
   inputColor: string
   translateZ: number
-  marginTop?: number
+  marginTop: number
+  height?: number
 }>`
   position: absolute;
   top: 0;
@@ -70,12 +86,30 @@ const ParallaxLayer = styled.div<{
 
   & > svg {
     margin-top: ${props => props.marginTop}vmin;
-    height: 50vw;
+    height: ${props => (props.height ? props.height : 50)}vw;
     width: 100vw;
     polygon {
       fill: ${props => props.inputColor};
     }
   }
+`
+const ParallaxRect = styled.div<{
+  inputColor: string
+  translateZ: number
+  marginTop: number
+  height: number
+}>`
+  position: absolute;
+  top: ${props => props.marginTop}vmin;
+  right: 0;
+  left: 0;
+
+  height: 100%;
+  background-color: ${props => props.inputColor};
+
+  transform: translateZ(-${props => props.translateZ}px)
+    scale(${props => 1 + props.translateZ / PERSPECTIVE});
+  z-index: -1;
 `
 
 const PageContent = styled.div`
